cb5b663766a8794fa4b0d7a2c8d05cf425288782
Aabdalah <115105760+Aabdalah@users.noreply.github.com>
2023-04-07 22:58:12 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+const popupButton = document.getElementById("popup-button");
+const popup = document.getElementById("popup");
+const popupClose = document.querySelector(".popup-close");
+
+popupButton.addEventListener("click", function() {
+  popup.style.display = "block";
+});
+
+popupClose.addEventListener("click", function() {
+  popup.style.display = "none";
+});
+
+let isDragging = false;
+let  lastY;
+
+popup.addEventListener("mousedown", function(e) {
+  isDragging = true;
+  lastY = e.clientY;
+});
+popup.addEventListener("touchstart", function(e) {
+  isDragging = true;
+  lastY = e.touches[0].clientY;
+});
+
+popup.addEventListener("mousemove", function(e) {
+  if (isDragging) {
+    let deltaY = e.clientY - lastY;
+    let rect = popup.getBoundingClientRect();
+    if(rect.top + deltaY>0){
+      popup.style.top = rect.top + deltaY + "px";
+    }
+    lastY = e.clientY;
+  }
+});
+popup.addEventListener("touchmove", function(e) {
+  if (isDragging) {
+    let deltaY = e.touches[0].clientY - lastY;
+    let rect = popup.getBoundingClientRect();
+    if(rect.top + deltaY>0){
+      popup.style.top = rect.top + deltaY + "px";
+    }
+    
+    lastY = e.touches[0].clientY;
+  }
+});
+
+popup.addEventListener("mouseup", function() {
+ 
+  console.log(lastY)
+  isDragging = false;
+  if(lastY>400){
+    popup.style.display='none';
+    popup.style.top=100+"px"
+  }
+  if(lastY<400 && lastY>100){
+    popup.style.top=100+"px"
+  }
+  if(lastY<100){
+    popup.style.top=0+"px";
+  }
+  
+});
+popup.addEventListener("touchend", function() {
+  console.log(lastY)
+  isDragging = false;
+  if(lastY>400){
+    popup.style.display='none';
+    popup.style.top=100+"px"
+  }
+  if(lastY<400 && lastY>100){
+    popup.style.top=100+"px"
+  }
+  if(lastY<100){
+    popup.style.top=0+"px";
+  }
+});
