c6cdc44d626b2943d9ffee72bf36a3ad48d07df2
Aabdalah <115105760+Aabdalah@users.noreply.github.com>
2023-04-08 09:46:39 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,81 +1,69 @@
 const popupButton = document.getElementById("popup-button");
 const popup = document.getElementById("popup");
 const popupClose = document.querySelector(".popup-close");
-
+const mover = document.getElementById("move");
+let startY;
 popupButton.addEventListener("click", function() {
+  popup.style.animation = "move 1s ease forwards";
   popup.style.display = "block";
+  popup.style.top = "50%";
+  
 });
 
 popupClose.addEventListener("click", function() {
   popup.style.display = "none";
+
 });
 popupClose.addEventListener("touchstart", function() {
   popup.style.display = "none";
+
 });
 
 let isDragging = false;
 let  lastY;
 
-popup.addEventListener("mousedown", function(e) {
+
+mover.addEventListener("mousedown", function(e) {
   isDragging = true;
   lastY = e.clientY;
+  popup.style.animation = "none";
+  
 });
-popup.addEventListener("touchstart", function(e) {
+mover.addEventListener("touchstart", function(e) {
+  
   e.preventDefault();
   isDragging = true;
   lastY = e.touches[0].clientY;
+  popup.style.animation = "none";
 });
 
 popup.addEventListener("mousemove", function(e) {
   if (isDragging) {
     let deltaY = e.clientY - lastY;
     let rect = popup.getBoundingClientRect();
-    if(rect.top + deltaY>0){
-      popup.style.top = rect.top + deltaY + "px";
-    }
+    popup.style.top = rect.top + deltaY + "px";
     lastY = e.clientY;
   }
 });
 popup.addEventListener("touchmove", function(e) {
+  
   e.preventDefault();
   if (isDragging) {
     let deltaY = e.touches[0].clientY - lastY;
     let rect = popup.getBoundingClientRect();
-    if(rect.top + deltaY>0){
-      popup.style.top = rect.top + deltaY + "px";
-    }
-    
+    popup.style.top = rect.top + deltaY + "px";
     lastY = e.touches[0].clientY;
   }
+  
 });
 
 popup.addEventListener("mouseup", function() {
  
-  console.log(lastY)
-  isDragging = false;
-  if(lastY>400){
-    popup.style.display='none';
-    popup.style.top=100+"px"
-  }
-  if(lastY<400 && lastY>100){
-    popup.style.top=100+"px"
-  }
-  if(lastY<100){
-    popup.style.top=0+"px";
-  }
-  
+  isDragging = false;  
+
 });
 popup.addEventListener("touchend", function() {
-  console.log(lastY)
   isDragging = false;
-  if(lastY>400){
-    popup.style.display='none';
-    popup.style.top=100+"px"
-  }
-  if(lastY<400 && lastY>100){
-    popup.style.top=100+"px"
-  }
-  if(lastY<100){
-    popup.style.top=0+"px";
-  }
+  
 });
+
